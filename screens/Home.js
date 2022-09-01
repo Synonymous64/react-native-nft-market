08247fe4ab4ca342873e18604e132dbf22b77eef
Synonymous64 <prajwalurkude007@gmail.com>
2022-09-01 16:17:53 +0530

    setup and components creation

---
+++ screens/Home.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import React from "react";
+import { Text, SafeAreaView, FlatList, View } from "react-native";
+import {COLORS, NFTData } from '../constants'
+const Home = () => {
+  return(
+    <SafeAreaView style={{flex: 1}}>
+      
+    </SafeAreaView>
+  )
+};
+
+export default Home;
