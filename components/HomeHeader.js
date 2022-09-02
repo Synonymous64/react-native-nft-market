cafc3b9473e51d7125321ccbdbee8376ad321d47
Synonymous64 <prajwalurkude007@gmail.com>
2022-09-02 16:26:27 +0530

    Layout and button design

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 const HomeHeader = () => {
   return (
     <View>
       <Text>HomeHeader</Text>
     </View>
-  )
-}
+  );
+};
 
-export default HomeHeader
+export default HomeHeader;
