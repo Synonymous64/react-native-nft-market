ecb886469e67f67f0d4c7c9cec2d045f5d9469c4
Synonymous64 <prajwalurkude007@gmail.com>
2022-10-10 19:58:42 +0530

    Details Updated a bit

--- screens/Details.js
+++ screens/Details.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Text,
   View,
@@ -15,11 +16,48 @@ import {
   DetailsDesc,
   DetailsBid,
 } from "../components";
+
+const DetailsHeader = ({ data, navigation }) => (
+  <View style={{ width: "100%", height: 373 }}>
+    <Image
+      source={data.image}
+      resizeMode="cover"
+      style={{ width: '100%', height: '100%' }}
+    />
+  </View>
+)
 const Details = ({ route, navigation }) => {
   // console.log(route);
   const { data } = route.params;
   console.log(data);
-  return <Text>Details</Text>;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
+      <View style={{ width: '100%', position: 'absolute', bottom: 0, paddingVertical: SIZES.font, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(255,255,255, 0.5)', zIndex: 1, }}>
+        <RectButton
+          minWidth={170}
+          fontSize={SIZES.large}
+          {...SHADOWS.dark}
+        />
+      </View>
+      <FlatList
+        data={data.bids}
+        renderItem={({ item }) => <DetailsBid bid={item} />}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: SIZES.extraLarge * 3 }}
+        ListHeaderComponent={() => (
+          <React.Fragment>
+            <DetailsHeader data={data} navigation={navigation} />
+          </React.Fragment>
+        )}
+      />
+    </SafeAreaView>
+  )
 };
 
 export default Details;
